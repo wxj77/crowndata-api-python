2354278fde14ffc6b70799922cbdee8c3648f60d
kingchou007 <jl3485@cornell.edu>
2024-10-15 02:52:01 +0800

    modify test path, add todo for cluster test

--- crowndata-evaluation/tests/crowndata_evaluation/services/action_consistency/test_clustering.py
+++ crowndata-evaluation/tests/crowndata_evaluation/services/action_consistency/test_clustering.py
@@ -52,7 +52,7 @@ def test_define_clusters(data, epsilon, expected_num_clusters):
 @pytest.mark.parametrize(
     "data, method_name, args, expected_num_clusters",
     [
-        # Edge case: large epsilon = 1 big cluster
+        # Edge case: all data points are the same, expect 1 cluster regardless of n_clusters
         [
             np.ones((100, 6)),
             "KMeans",
@@ -61,7 +61,7 @@ def test_define_clusters(data, epsilon, expected_num_clusters):
             },
             1,
         ],
-        # Edge case: small epsilon = 100 small clusters
+        # Normal case: random data, expect the specified number of clusters
         [
             np.random.rand(100, 6),
             "KMeans",
@@ -70,6 +70,7 @@ def test_define_clusters(data, epsilon, expected_num_clusters):
             },
             20,
         ],
+        # Case with some duplicate points, expect fewer clusters than specified
         [
             np.array(
                 [
@@ -85,6 +86,7 @@ def test_define_clusters(data, epsilon, expected_num_clusters):
             },
             3,
         ],
+        #TODO: Edge case: empty data
     ],
 )
 def test_sklearn_cluster_wrapper(data, method_name, args, expected_num_clusters):
